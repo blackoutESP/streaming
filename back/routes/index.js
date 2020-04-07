ef3b97ae0cb2c982836ca805a32ce48b1ad77416
yxion <yxion10101@gmail.com>
2020-04-07 20:49:58 +0200

    fixed formidable compatibility bug

--- back/routes/index.js
+++ back/routes/index.js
@@ -56,18 +56,28 @@ router.post('/upload', async(request, response, next)=>{
     data.type           = true;
     data.uploadDir      = path.join(__dirname, '../assets/');
     data.keepExtensions = true;
-    data.on('file', async(file, field)=>{
-        fs.rename(file.path, data.uploadDir + file.name, (error)=>{
-            if(error){
-                console.error(error);
-            }
+    /*
+        data.on('file', async(error, fields, file)=>{
+            console.log(file);
+            fs.rename(file.path, `${file.path}/${file.name}`, (error)=>{
+                if(error){
+                    console.error(error);
+                }
+            });
         });
-    });
+    */
     data.on('end', ()=>{
         // send response to user
     });
-    data.parse(request, ()=>{
-        return response.status(201).json({ok: true, data: [{msg: 'file uploaded correctly.'}], fails: []});
+    data.parse(request, (err, fields, files)=>{
+        //console.log(files.test);
+        fs.rename(files.test.path, path.join(__dirname, '../assets/', files.test.name), (error)=>{
+            if(error){
+                console.error(error);
+                //return response.status(400).json({ok: false, data: [{msg: error}], fails: []});
+            }
+            return response.status(201).json({ok: true, data: [{msg: 'file uploaded correctly.'}], fails: []});
+        });
     });
 });
 
