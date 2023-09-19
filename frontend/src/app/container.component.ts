e522b4e4e0b7c7cc16594a1ee07675eb91ae50b5
blackoutESP <{ID}+{yxion10101@gmail.com}@users.noreply.github.com>
2023-09-19 23:09:30 +0200

    margins on matlistitems and spinner color

--- frontend/src/app/container.component.ts
+++ frontend/src/app/container.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, Output, VERSION } from '@angular/core';
+import { Component, Input, OnInit, Output, VERSION } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 import { LoginService } from './services/login.service';
 import { VideosService } from './services/videos.service';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
@@ -13,15 +14,26 @@ export class Container implements OnInit {
 
   public title = 'Small Streaming Service';
   public version = '2.0-1';
-  @Output() themeSelected: BehaviorSubject<string> = new BehaviorSubject('Dark');
+  private overlay: any;
+  @Input() public overlayTheme = new BehaviorSubject<string>('dark-theme');
+  @Output() themeSelected: BehaviorSubject<string> = new BehaviorSubject('dark-theme');
   @Output() checked: BehaviorSubject<boolean> = new BehaviorSubject(true);
-  public loaded: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  public loaded: BehaviorSubject<boolean> = new BehaviorSubject(true);
   public videos: string[] = [];
   private token: string = '';
+  public mobile: boolean = false;
   constructor(
+    private overlayContainer: OverlayContainer,
     private router: Router,
     private loginService: LoginService,
     private videosService: VideosService) {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|CriOS/i.test(navigator.userAgent)) {
+      this.mobile = true;
+    } else {
+      this.mobile = false;
+    }
+    this.overlay = this.overlayContainer.getContainerElement();
+    console.log(this.overlayContainer.getContainerElement());
   }
 
   // this.router.navigate(['videos/'], { skipLocationChange: true });
@@ -53,10 +65,16 @@ export class Container implements OnInit {
 
   switchTheme(event: any): void {
     if (!event.checked) { // Dark theme
-      this.themeSelected.next('Dark');
+      this.overlay.classList.remove('light-theme');
+      this.overlay.classList.add('dark-theme');
+      this.overlayTheme.next('dark-theme');
+      this.themeSelected.next('dark-theme');
       this.checked.next(true);
     } else { // Light theme
-      this.themeSelected.next('Light');
+      this.overlay.classList.remove('dark-theme');
+      this.overlay.classList.add('light-theme');
+      this.overlayTheme.next('light-theme');
+      this.themeSelected.next('light-theme');
       this.checked.next(false);
     }
   }
