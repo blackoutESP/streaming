e34092148aaa3acf6d5e4be7c637ea8f6997c14c
blackoutESP <{ID}+{yxion10101@gmail.com}@users.noreply.github.com>
2023-09-19 17:59:20 +0200

    .

--- frontend/src/app/env/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  loginUrl: 'http://0.0.0.0:3000/api/login',
-  videosUrl: 'http://0.0.0.0:3000/api/videos/',
-  videoById: 'http://0.0.0.0:3000/api/videos/',
-  port: 3000
-};
--- frontend/src/app/interfaces/theme-selected.enum.ts
+++
@@ -1,4 +0,0 @@
-export const Themes = {
-  DarkTheme: 'Dark',
-  LightTheme: 'Light'
-}
